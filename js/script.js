072ec72f365353935904efe920edd524f95d71a6
Rahul636390 <rahuls2005student@gmail.com>
2025-12-23 13:00:36 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://portfolio-backend-1-0mrt.onrender.com"; // local
+const API_URL = "https://portfolio-backend-1-0mrt.onrender.com/contact"; // local
 // const API_URL = "https://your-backend.onrender.com/contact"; // production
 
 const toggleBtn = document.getElementById("themeToggle");
@@ -44,4 +44,5 @@ form.addEventListener("submit", async (e) => {
         statusText.style.color = "red";
     }
 });
+
 
